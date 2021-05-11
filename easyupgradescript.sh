f8af3ddac960c727a6a075bd2bb1003af96bdd46
Jordan Caroleo <jordan.caroleo91@gmail.com>
2021-05-11 18:03:51 +0000

    init

---
+++ easyupgradescript.sh
@@ -0,0 +1,18 @@
+purple="\e[1;35;40m"
+endcolor="\e[0m"
+green="\e[1;32;40m"
+
+sudo apt update
+sudo apt upgrade
+echo -e "${green}Updates completed,${endcolor}${purple}$USER!${endcolor}"
+
+
+
+
+
+
+
+
+
+
+
