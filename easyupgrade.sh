36d8ee468e72ac449facaa252866aef98ef08cdd
jordar411 <38548084+jordar411@users.noreply.github.com>
2021-05-11 14:32:15 -0400

    "Yes" Command Update
    Added "yes" command will automatically print "y" to allow updates to installed without review. Please remove "yes" command to manually confirm updates before installing.

--- easyupgrade.sh
+++ easyupgrade.sh
@@ -3,7 +3,9 @@ endcolor="\e[0m"
 green="\e[1;32;40m"
 
 sudo apt update
-sudo apt upgrade
+#remove "yes" command below if you want to manually confirms updates before installing
+yes | sudo apt upgrade
+
 echo -e "${green}Updates completed, ${endcolor}${purple}$USER!${endcolor}"
 
 
